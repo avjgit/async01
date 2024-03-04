bee732c85f0196f37dec6de009b33402b5164574
A J <avjmail@gmail.com>
2024-03-04 17:08:34 +0200

    tasks api

---
+++ Tasks/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace Tasks;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string? Assignee { get; set; }
+    public bool IsComplete { get; set; }
+}
