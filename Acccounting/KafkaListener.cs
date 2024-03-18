0ca67e04ac57fc13195d4b93e9b03b7fdda043aa
A J <avjmail@gmail.com>
2024-03-18 14:19:55 +0200

    send errors to Dead letter queue

--- Acccounting/KafkaListener.cs
+++ Acccounting/KafkaListener.cs
@@ -1,5 +1,6 @@
 ﻿using Accounting;
 using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
 
 namespace Acccounting;
 
@@ -7,16 +8,23 @@ public class KafkaListener : BackgroundService
 {
     private readonly ILogger<KafkaListener> _logger;
     private readonly IConsumer<String, AccountingTask> _consumer;
+    private readonly IProducer<String, AccountingTask> _producer;
     private const String Topic = "tasksSchemed";
+    private readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
 
-    public KafkaListener(IConsumer<String, AccountingTask> consumer, ILogger<KafkaListener> logger)
+    public KafkaListener(
+        IConsumer<String, AccountingTask> consumer, 
+        IProducer<string, AccountingTask> producer,
+        ILogger<KafkaListener> logger)
     {
         _logger = logger;
         _consumer = consumer;
+        _producer = producer;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _producer.InitTransactions(DefaultTimeout);
         _consumer.Subscribe(Topic);
 
         while(!stoppingToken.IsCancellationRequested)
@@ -30,8 +38,19 @@ public class KafkaListener : BackgroundService
     }
 
     protected virtual async Task HandleMessage(AccountingTask task, CancellationToken stoppingToken)
-    {
-        _logger.LogInformation($"Message Received: {task.Id}|{task.Assignee}");
+    {        
+        try
+        {
+            _logger.LogInformation($"Message Received: {task.Id}|{task.Assignee}"); // handle all the logic
+        }
+        catch (Exception ex)
+        {
+            var message = new Message<String, AccountingTask>
+            {
+                Value = task
+            };
+            _producer.ProduceAsync("tasksDeadLetterQueue", message, stoppingToken);
+        }
 
         await Task.CompletedTask;
     }
